a8b4ec4fa879626db8b231affb094bdda50a3029
DurianSuen <durian.suen@gmail.com>
2014-07-07 06:35:26 +0800

    create web project

---
+++ web project/reslibadmin/src/test/java/com/uce360/reslibadmin/test/service/TestUserService.java
@@ -0,0 +1,12 @@
+package com.uce360.reslibadmin.test.service;
+
+import org.apache.log4j.Logger;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring.xml", "classpath:spring-mybatis.xml" })
+public class TestUserService {
+	private static final Logger logger = Logger.getLogger(TestUserService.class);
+}
