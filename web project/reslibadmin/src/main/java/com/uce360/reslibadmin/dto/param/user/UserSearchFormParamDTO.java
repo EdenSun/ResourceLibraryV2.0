fc7b5c1321b7cdf8b0fab0ed8f6d14a7d3ccbe12
DurianSuen <durian.suen@gmail.com>
2014-07-29 22:42:41 +0800

    modify

---
+++ web project/reslibadmin/src/main/java/com/uce360/reslibadmin/dto/param/user/UserSearchFormParamDTO.java
@@ -0,0 +1,61 @@
+package com.uce360.reslibadmin.dto.param.user;
+
+public class UserSearchFormParamDTO {
+	private String account;
+	private String realname;
+	private Integer sex;
+	private String email;
+	private Integer roleType;
+	private String studentId;
+	private String startYear;
+	private String mobilePhone;
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public Integer getSex() {
+		return sex;
+	}
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getRoleType() {
+		return roleType;
+	}
+	public void setRoleType(Integer roleType) {
+		this.roleType = roleType;
+	}
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getStartYear() {
+		return startYear;
+	}
+	public void setStartYear(String startYear) {
+		this.startYear = startYear;
+	}
+	public String getMobilePhone() {
+		return mobilePhone;
+	}
+	public void setMobilePhone(String mobilePhone) {
+		this.mobilePhone = mobilePhone;
+	}
+	
+}
